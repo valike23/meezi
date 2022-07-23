ef33310698e023ed9fd63501ca7f9ba4ff2eadaa
ludike23 <ludike23@gmail.com>
2022-07-23 07:49:46 +0100

    mezzi

---
+++ src/Model/chat.ts
@@ -0,0 +1,14 @@
+
+export enum EdiscussionScreen {
+    EMPTY = 0,
+    CHAT = 1,
+    REQUEST = 2
+}
+export enum EchatMessageType {
+    TEXT = 0,
+    IMAGE = 1,
+    AUDIO =2,
+    VIDEO = 3,
+    DOCUMENT = 4,
+
+}
